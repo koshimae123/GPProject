9d34c9bfd2a42098941f68a831f2c9cf98b3b5a0
STD\s20217052 <20217052-koshimaeryohei@hcs.ac.jp>
2022-12-06 15:01:50 +0900

    会員修正を追加

--- 20217052/20217052/Form4.cs
+++ 20217052/20217052/Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace _20217052
 {
@@ -25,6 +26,33 @@ namespace _20217052
             
             //画面を閉じる
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using(SQLiteConnection con = new SQLiteConnection("Data Source=table.db"))
+            {
+                con.Open();
+                using (SQLiteTransaction trans = con.BeginTransaction())
+                {
+                    SQLiteCommand cmd = con.CreateCommand();
+                    //インサート
+                    cmd.CommandText = "UPDATE FROM HCS_product set name = @name, address = @address, phone_number = phone_number WHERE member_id = @member_id";
+                    //パラメータセット
+                    cmd.Parameters.Add("name", System.Data.DbType.String);
+                    cmd.Parameters.Add("address", System.Data.DbType.String);
+                    cmd.Parameters.Add("phone_number", System.Data.DbType.Int64);
+                    cmd.Parameters.Add("member_id", System.Data.DbType.Int64);
+                    //データ追加
+                    cmd.Parameters["name"].Value = textBox1.Text;
+                    cmd.Parameters["address"].Value = textBox2.Text;
+                    cmd.Parameters["phone_number"].Value = int.Parse(textBox3.Text);
+                    cmd.Parameters["member_id"].Value = int.Parse(textBox4.Text);
+                    cmd.ExecuteNonQuery();
+                    //コミット
+                    trans.Commit();
+                }
+            }
         }
     }
 }
